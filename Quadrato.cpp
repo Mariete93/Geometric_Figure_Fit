4286516e66e3722c7e32a167ed640776c5d10e19
Mario Motta <89982734+Mariete93@users.noreply.github.com>
2022-04-08 09:41:16 +0200

    Add files via upload

---
+++ Quadrato.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+def retta1
+
+
+int main()
+{
+	cout << "Hello World";
+	double points[10][10];
+	points= [[]];
+	double Chi2=0
+	sigma = 2;
+	for i=0; i<length(points)\\2; i++;
+		Chi2+=pow(points[i][1]-retta1(points[i][0]),2);
+	for j = length(points)\\2-1 ; j<length(points); j++;
+		Chi2+=pow(points[j][1]-retta2(points[j][0]),2);
+	Chi2=Chi2/pow(sigma,2)
+	
+}
+
